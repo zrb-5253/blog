13683377212a4403653c161ac3eea4e0cfaf7712
zrb <2066415531@qq.com>
2025-09-29 20:07:17 +0800

    打字机2

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,33 +1,9 @@
-// .vuepress/client.js
-import { defineClientConfig } from '@vuepress/client'
+import { defineClientConfig } from "vuepress/client";
+import Blog from "./layouts/Blog.vue";
 
 export default defineClientConfig({
-  enhance({ app, router, siteData }) {
-    if (typeof window !== 'undefined') {
-      // 在客户端执行
-      window.addEventListener('load', () => {
-        const tagline = document.querySelector('.tagline')
-        if (tagline) {
-          const text = tagline.textContent
-          tagline.innerHTML = `<span class="typewriter-text">${text}</span>`
-          
-          // 添加打字机效果
-          let i = 0
-          const speed = 100
-          const typewriterElement = tagline.querySelector('.typewriter-text')
-          typewriterElement.textContent = ''
-          
-          function typeWriter() {
-            if (i < text.length) {
-              typewriterElement.textContent += text.charAt(i)
-              i++
-              setTimeout(typeWriter, speed)
-            }
-          }
-          
-          typeWriter()
-        }
-      })
-    }
-  }
-})
+  layouts: {
+    // 注册自定义的Blog布局
+    Blog,
+  },
+});
---
+++ src/.vuepress/layouts/Blog.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Blog } from "vuepress-theme-hope/blog";
+import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+</script>
+
+<template>
+  <Blog>
+    <template #heroInfo="data">
+      <HitokotoBlogHero v-bind="data" />
+    </template>
+  </Blog>
+</template>
