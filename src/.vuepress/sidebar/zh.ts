0ec619612383a83e74b2c99d34c77a4ae0922178
zrb <2066415531@qq.com>
2025-09-29 14:32:08 +0800

    code补充

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -39,4 +39,60 @@ export const zhSidebar = sidebar({
       children: "structure", // 自动生成 `notes/` 目录下的所有子项
     },
   ],
+  "/zh/code/": [
+    "", // 这对应 `/zh/book/README.md` 文件，通常作为目录页
+    {
+      text: "c++",
+      icon: "book", // 可以选择一个合适的图标
+      prefix: "book/",
+      link: "demo/",
+      children: "structure", // 自动生成 `books/` 目录下的所有子项
+    },
+    {
+      text: "python",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+    {
+      text: "Markdown",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+    {
+      text: "stm32",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+    {
+      text: "linux",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+    {
+      text: "QT",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+  ],
+  "/zh/stduy/": [
+    "", // 这对应 `/zh/book/README.md` 文件，通常作为目录页
+    {
+      text: "书籍推荐",
+      icon: "book", // 可以选择一个合适的图标
+      prefix: "book/",
+      link: "demo/",
+      children: "structure", // 自动生成 `books/` 目录下的所有子项
+    },
+    {
+      text: "读书笔记",
+      icon: "note",
+      prefix: "notes/",
+      children: "structure", // 自动生成 `notes/` 目录下的所有子项
+    },
+  ],
 });
