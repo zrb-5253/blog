80c0b0430ac11ec73ba0877913db5e92a3ae0015
zrb <2066415531@qq.com>
2025-10-04 11:53:29 +0800

    删每日

--- src/.vuepress/layouts/Blog.vue
+++ src/.vuepress/layouts/Blog.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { Blog } from "vuepress-theme-hope/blog";
-import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+
 </script>
 
 <template>
   <Blog>
-    <template #heroInfo="data">
-      <HitokotoBlogHero v-bind="data" />
-    </template>
+
   </Blog>
 </template>
